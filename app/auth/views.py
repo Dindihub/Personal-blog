ecdb55a2d9c0b93af5f1ed2572faa5c2ee88f5b4
Dindihub <sadindi03@gmail.com>
2022-05-16 00:03:23 +0300

    Code-add routes for login and registration page

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,11 @@
 from flask import render_template,redirect,url_for, flash,request
 from flask_login import login_user,logout_user,login_required
-from ..models import User,Wallet,Transaction
-from .forms import LoginForm,RegistrationForm
+from ..models import User,Subscriber
+from .forms import LoginForm,SignupForm
 from .. import db
 from . import auth
 
-@auth.route('/login',methods=['GET','POST'])
+@auth.route('/auth/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
@@ -16,20 +16,20 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
-    return render_template('auth/login.html',login_form = login_form,title=title)
+  
+    return render_template('auth/login.html',form =login_form)
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
+@auth.route('/auth/signup',methods = ["GET","POST"])
+def signup():
+    form = SignupForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data,password_confirm=form.password_confirm.data)
         db.session.add(user)
         db.session.commit()
       
         return redirect(url_for('auth.login'))
         title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
+    return render_template('auth/signup.html',form = form)
 
 @auth.route('/logout')
 @login_required
